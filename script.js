a8f5ba971b01addd74ff08678b637cc48c5b503e
alexboelle <boelle@MBPdealxboelle2.lan>
2024-11-17 20:16:25 +0100

    Premier commit - création du site galerie

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script de base pour les interactions (ex: formulaire, pop-ups...)
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Merci pour votre message !');
+  });
+
+  
